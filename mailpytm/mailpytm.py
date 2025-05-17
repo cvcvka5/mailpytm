bac988407b9559b90ae744961091b57b44152560
Ulus Vatansever <cvcvka5@gmail.com>
2025-05-17 20:01:54 +0300

    Fixed imports

--- mailpytm/mailpytm.py
+++ mailpytm/mailpytm.py
@@ -2,7 +2,7 @@ from typing import Dict, List
 import requests
 import random
 import string
-from exceptions import RegistrationFailed, TooManyRequests, TokenError, FetchMessagesFailed, FetchAccountFailed
+from .exceptions import RegistrationFailed, TooManyRequests, TokenError, FetchMessagesFailed, FetchAccountFailed
 import time
 
 

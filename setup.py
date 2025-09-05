154f80d20caf569ea9e2bab66ad723c8aa4a595d
cvcvka5 <cvcvka5@gmail.com>
2025-09-05 13:33:13 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mailpytm",                     # Your package name on PyPI
-    version="1.0.0",                   # Initial version
-    author="cvcvka5",
-    author_email="cvcvka5@gmail.com",
+    name="mailpytm",
+    version="1.1.0",
+    author="Ulus Vatansever",
+    author_email="ulusvatansever@gmail.com",
     description="A Python client for the mail.tm temporary email service API",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/cvcvka5/mailpytm",  # Replace with your repo URL
-    packages=find_packages(),          # Finds mailtm and any other packages
+    url="https://github.com/cvcvka5/mailpytm",
+    packages=find_packages(),
     python_requires='>=3.7',
     install_requires=[
         "requests>=2.0.0",
